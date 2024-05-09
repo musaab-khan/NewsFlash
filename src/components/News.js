f0553c8e9622881d93fef9aca8f67f514d15a729
musaab-khan <musaabkhan4511@gmail.com>
2024-05-09 09:25:34 +0500

    infinite scrolling

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Loader from './Loader';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
   static defaultProps ={
@@ -15,75 +16,115 @@ export class News extends Component {
     category: PropTypes.string
   }
   articles = []
-constructor(){
-  super(); //parent's constructor (has to be called)
+
+  capitalizeFirstLetter=(word)=> {
+    if (word && typeof word === 'string') {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    } else {
+      return '';
+    }
+  }
+
+constructor(props){ //using props bcz using category from props in constructor
+  super(props); //parent's constructor (has to be called)
   this.state={
     articles:this.articles,
     loading : false,
-    page: 1
+    page: 1,
+    totalResults: 0
   }
-}
-async componentDidMount(){
-  let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=48bb98f2a6c0458d873f0244eac7f043&page=1&pageSize=${this.props.pageSize}`;
-  this.setState({loading:true});
-  let data = await fetch(url);
-  let parsedData= await data.json();
-  console.log(parsedData);
-  this.setState({
-    articles: parsedData.articles,
-    totalResults: parsedData.totalResults,
-    loading: false
-  })
+  document.title=`${this.capitalizeFirstLetter(this.props.category)} - NewsFlash`;
 }
 
-  handlePreviousClick=async ()=>{
-    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=48bb98f2a6c0458d873f0244eac7f043&page=${this.state.page-1}&pageSize=${this.props.pageSize}`
+async newsUpdate(){
+  let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=48bb98f2a6c0458d873f0244eac7f043&page=${this.state.page}&pageSize=${this.props.pageSize}`
     this.setState({loading: true})
   let data = await fetch(url);
   let parsedData= await data.json();
-  console.log(parsedData);
     this.setState({
-      page: this.state.page-1,
       articles: parsedData.articles,
-      loading: false
+      totalResults: parsedData.totalResults,
+      loading: false,
+      page: this.state.page
     })
-  }
-  handleNextClick=async()=>{
-    console.log("next")
-    if(!(this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize))){
-    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=48bb98f2a6c0458d873f0244eac7f043&page=${this.state.page+1}&pageSize=${this.props.pageSize}`
-    this.setState({loading:true});
+    
+}
+
+async componentDidMount(){
+  this.newsUpdate();
+}
+
+  // handlePreviousClick= ()=>{
+  //   this.setState(prevState => ({page: prevState.page-1}), () => {
+  //     this.newsUpdate();
+  //   });
+  // }
+  // handleNextClick=()=>{
+  //   this.setState(prevState => ({page: prevState.page+1}), () => {  //here callback function prevState is used bcz Since setState is asynchronous, without a callback function, there is no guarantee that the state has been updated when you try to access it immediately after calling setState.
+  //     this.newsUpdate();
+  //   });
+  // }
+  
+  fetchData = async () => {
+    this.setState({ loading: true });
+  
+    // Fetch data with the updated page number
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=48bb98f2a6c0458d873f0244eac7f043&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+  
     let data = await fetch(url);
-    let parsedData= await data.json();
-    console.log(parsedData);
-    this.setState({
-      page: this.state.page+1,
-      articles: parsedData.articles,
-      loading: false
-    })
-  }
-  }
+    let parsedData = await data.json();
+  
+    // Update state using the callback function
+    this.setState(prevState => ({
+      articles: [...prevState.articles, ...parsedData.articles],
+      totalResults: parsedData.totalResults,
+      loading: false,
+      page: prevState.page + 1
+    }));
+  };
+  
+
 
   render() {
-    console.log("render")
     return (
-      <div className="container my-3">
-        <h2 className='text-center'>NewsFlash - Top Head Lines</h2>
+      <>
+        <h1 className='text-center'>NewsFlash - Top {this.capitalizeFirstLetter(this.props.category)} Head Lines</h1>
         {this.state.loading&&<Loader/>}
-        {!(this.state.loading) && <div className="row">
-          {this.state.articles.map((e)=>{
-            return (
-              <div className='col md-4' key={e.url}>
-                <NewsItem title={e.title?e.title.slice(0,45):""} description={e.description?e.description.slice(0,88):""} imgUrl={e.urlToImage} newsUrl={e.url}></NewsItem>
-              </div>
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length} //This is important field to render the next data
+          next={this.fetchData}
+          hasMore={this.state.articles.length!=this.state.totalResults}
+          loader={<Loader/>}
+          endMessage={
+            this.state.articles.length == this.state.totalResults ? (
+              <p style={{ textAlign: 'center' }}>
+                <b>Yay! You have seen it all</b>
+              </p>
+            ) : null
+          }
+          
+        >
+          <div className="container">
+          <div className="row">
+            {this.state.articles.map((e)=>{
+               return (
+                <div className='col md-4' key={e.url}>
+                <NewsItem title={e.title?e.title.slice(0,45):""} description={e.description?e.description.slice(0,88):""} imgUrl={e.urlToImage} newsUrl={e.url} author={e.author} date={e.publishedAt} source={e.source.name}> </NewsItem>
+            </div>
             )
           })}
-        </div>}
-        <div class="d-flex justify-content-between">
+        </div>
+        
+
+        </div>
+        </InfiniteScroll>
+        
+        {/* <div class="d-flex justify-content-between">
                 <button disabled={this.state.page<=1} type="button" className="btn btn-info" onClick={this.handlePreviousClick}>&larr; Previous</button>
                 <button disabled={this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-info" onClick={this.handleNextClick}>Next &rarr;</button>
-        </div>
-      </div>
+        </div> */}
+      </>
 
     )
     
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title,description,imgUrl,newsUrl}=this.props;
+    let {title,description,imgUrl,newsUrl,author,date, source}=this.props;
+    const gmtDateString = new Date(date).toGMTString();
     return (
       <div>
         <div className="card my-3" style={{width: "18rem"}}>
             <img src={imgUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
+                <h5 className="card-title">{title} <span class="badge text-bg-danger rounded-pill"style={{position: 'absolute', top: '-2%', right: '-15px'}}>{source}</span></h5>
                 <p className="card-text">{description}</p>
+                <p><small className='text-muted'>By {!author?"Unknown":author} at {gmtDateString}</small></p>
                 <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Expand</a>
             </div>
             </div>
